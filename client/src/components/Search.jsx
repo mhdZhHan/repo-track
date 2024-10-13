8fc477adde7f878e9475d21ec05fbe6d8bf07ba1
Mohammed <dev.mohammedsh@gmail.com>
2024-10-13 22:43:28 +0530

    ✨ Fix search style

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -2,7 +2,7 @@ import { Search as SearchIcon} from "lucide-react"
 
 const Search = () => {
 	return (
-		<form className="max-w-xl mx-auto p-2 sm:w-72">
+		<form className="max-w-xl mx-auto p-2">
 			<label
 				htmlFor="default-search"
 				className="mb-2 text-sm font-medium text-gray-900 sr-only"
